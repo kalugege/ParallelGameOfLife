6779d194eb38f69e43c993790ef7cc1805a6ec3d
kalugege <kalugege@gmail.com>
2020-11-01 14:54:58 +0100

    task4

--- igra_zivota_zadatak3.py
+++ igra_zivota_zadatak3.py
@@ -1,67 +1,129 @@
 import numpy as np
 import multiprocessing as mp
+import queue
+import time
 
 N = 4
+brojCelija = mp.Value('i',0)
 matrica=np.random.randint(2,size=(N,N))
-
-def igra_zivota(task,matrica,N):
+brojCelijaCondition = mp.Condition()
+class Celija(mp.Process):
+    def __init__(self,stanje,i,j,brojCelija,redPoruka,brojCelijaCondition):
+        super().__init__()
+        self.stanje=stanje      
+        self.i=i
+        self.j=j
+        self.iteracija=0
+        self.brojCelija=brojCelija
+        self.stanjeKomsija = mp.Queue()
+        self.redPoruka=redPoruka
+        self.brojCelijaCondition=brojCelijaCondition
     
-    koordinate = {}
-    for x , y in task:
-        ziveKomsije=0
-        for i in range(x - 1, x + 2):
-            for j in range(y - 1,y + 2):
-                if x == i and y == j:
+    
+    def proveraSuseda(self):
+        for x in range(self.i - 1, self.i + 2):
+            for y in range(self.j - 1, self.j + 2):
+                if x == self.i and y == self.j:
                     continue
-                if i<0 or i>= N:
-                    i=i % N
-                if j < 0 or j>=N:
-                    j = j % N
-                ziveKomsije += matrica[i,j]
-        # print(ziveKomsije)
+                
+                if x<0 or x>= N:
+                    x=x % N
+                if y < 0 or y>=N:
+                    y = y % N
+                self.azurirajQueue(x,y)    
+
+    def azurirajQueue(self,x,y):
+        for celija in listaCelija:
+            if celija.i == x and celija.j == y:
+                celija.stanjeKomsija.put(self.stanje)
+
+    def promenaStanja(self,ziveKomsije):
+        global matrica
+        
         if ziveKomsije < 2 or ziveKomsije > 3:
-            koordinate[(x,y)]=0
-        elif matrica[x,y] == 1 and (ziveKomsije == 2 or ziveKomsije == 3):
-            koordinate[(x,y)]=1
-        elif matrica[x,y] == 0 and ziveKomsije == 3:
-           koordinate[(x,y)]=1
-        else: koordinate[(x,y)]=0          
+            self.stanje = 0
+            
+        elif self.stanje == 1 and (ziveKomsije == 2 or ziveKomsije == 3):
+            self.stanje = 1
+           
+        elif self.stanje == 0 and ziveKomsije == 3:
+            self.stanje = 1
+            
 
-    return koordinate
-if __name__ == '__main__':
-    lista_matrica=[matrica.copy()]
-    tasks=[]
-    rezultati=[]
-    # print(matrica)
+        
+        
+        
+        self.iteracija+=1 
+        self.redPoruka.put((self.i,self.j,self.iteracija,self.stanje))
 
-    for i in range(N):
-        red = []
-        for j in range(N):
-            red.append((i,j))
-        tasks.append(red)
+    def run(self):
+        for _ in range(0,4):
+            self.proveraSuseda()
+            ziveKomsije=0
+            for _ in range(0,8):
+                ziveKomsije+=self.stanjeKomsija.get()
+            time.sleep(0.1)
+            self.promenaStanja(ziveKomsije)
 
-    pool = mp.Pool(mp.cpu_count())
-
-    for i in range(0,100):
-        rezultati = [pool.apply(igra_zivota,args=(task,matrica,N)) for task in tasks]
-
-        for rezultat in rezultati:
-            for kljuc,vrednost in rezultat.items():
-                matrica[kljuc[0],kljuc[1]]=vrednost
-                # print(kljuc , ':' , vrednost)
-
-        lista_matrica.append(matrica.copy()) 
-    # print(rezultati)
-    pool.close()
-    pool.join()
-count=0
-for m in lista_matrica:
-
-    print(count,m)
-    count=count+1
-
-
-                    
+           
+            with brojCelija.get_lock():
+                self.brojCelija.value+=1
+            if self.brojCelija.value == N * N:
+                # self.brojCelija.get_lock().acquire()
+                # self.brojCelija.value=0
+                # self.brojCelija.get_lock().release() 
+                with brojCelija.get_lock():
+                    self.brojCelija.value=0  
                 
                 
+                self.brojCelijaCondition.acquire()
+                self.brojCelijaCondition.notify_all()
+                self.brojCelijaCondition.release()
+
+            else:
+                
+                self.brojCelijaCondition.acquire()
+                self.brojCelijaCondition.wait()
+                self.brojCelijaCondition.release()    
+
+
+        
+class Servis(mp.Process):
+    def __init__(self,matrica,redPoruka,listaMatrica):
+        super().__init__()
+        self.matrica=matrica
+        self.redPoruka=redPoruka
+        self.listaMatrica=listaMatrica                    
+
+    def run(self):
+        for _ in range(0,4):
+            for _ in range(0,N*N):
+                poruka = self.redPoruka.get()
+                
+                self.matrica[poruka[0],poruka[1]]=poruka[3]
+
+            self.listaMatrica.append(matrica.copy())     
+
+        for m in listaMatrica:
+           print(m)
+
+redPoruka = mp.Queue()
+listaMatrica=[]
+servis=Servis(matrica,redPoruka,listaMatrica)
+servis.start()
+
+listaCelija=[]         
+for i in range(0,N):
+    for j in range(0,N):
+        celija = Celija(matrica[i,j],i,j,brojCelija,redPoruka,brojCelijaCondition)
+        listaCelija.append(celija)     
+ 
+for celija in listaCelija:
+    celija.start()
+for celija in listaCelija:
+    celija.join()
+
+
+servis.join()                      
+
 
